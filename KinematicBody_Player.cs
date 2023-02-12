656a47d59e930d6a05f27916e01accd80048f829
BearEdelweis <110041849+BearEdelweis@users.noreply.github.com>
2023-02-12 21:26:28 +0800

    with touch

--- KinematicBody_Player.cs
+++ KinematicBody_Player.cs
@@ -5,26 +5,37 @@ public class KinematicBody_Player : KinematicBody
 {
     [Signal] public delegate void Hit();
 
-    [Export] private int fallAcceleration; //имитация свободного падения
-    [Export] private int speed; //скорость игрока
-    [Export] private int jumpImpuls;
-    [Export] private int bounceImpuls;
+    private int fallAcceleration; //имитация свободного падения
+    private int speed; //скорость игрока
+    private int jumpImpuls;
+    private int bounceImpuls;
     private Vector3 velocity; //направление движения
     private Spatial spatialPivot;
+
+    private bool waitForDoubleTap;
+    private bool isJumping;
+
+    private Timer doubleTapAwaiter;
     
     public override void _Ready()
     {
         fallAcceleration = 75;
-        speed = 14;
+        speed = 5;
         jumpImpuls = 20;
         bounceImpuls = 16;
         velocity = Vector3.Zero;
         spatialPivot = GetNode<Spatial>("Spatial_Pivot");
+        waitForDoubleTap = false;
+        isJumping = false;
+        doubleTapAwaiter = GetNode<Timer>("DoubleTapAwaiter");
     }
 
     private void Die()
     {
         EmitSignal(nameof(Hit));
+        //Hide();
+        //if()
+        //await ToSignal(GetTree().CreateTimer(0.3f), "timeout");
         QueueFree();
     }
 
@@ -33,32 +44,99 @@ public class KinematicBody_Player : KinematicBody
         Die();
     }
 
+    public override async void _Input(InputEvent iE)
+    {
+        if(iE is InputEventScreenTouch sT)
+        {
+            if(sT.Pressed && !waitForDoubleTap)
+            {
+                waitForDoubleTap = true;
+                doubleTapAwaiter.Start();
+                await ToSignal(doubleTapAwaiter, "timeout");
+                waitForDoubleTap = false;
+                //GD.Print(waiter.GetType());
+            }
+            if(sT.Pressed && waitForDoubleTap) //&& IsOnFloor())
+            {
+                isJumping = true;
+                //velocity.y += jumpImpuls;
+                GD.Print("DoubleTap!");
+                waitForDoubleTap = false;
+            }
+            
+            if(!sT.Pressed)
+            {
+                //velocity.x = 0;
+                velocity = Vector3.Zero;
+            }
+        }
+        
+        if (iE is InputEventScreenDrag sD)
+        {
+            velocity.x = sD.Speed.x;
+            velocity.z = sD.Speed.y;
+        }
+    }
+
+    private void ConstraintsPlayerMove()
+    {
+        Vector3 pos = Translation;
+
+        if (Translation.z < -12)
+        {
+            pos.z = -12;
+            Translation = pos;
+        }
+        if (Translation.z > 14)
+        {
+            pos.z = 14;
+            Translation = pos;
+        }
+
+        if (Translation.x < -5)
+        {
+            pos.x = -5;
+            Translation = pos;
+        }
+        if (Translation.x > 5)
+        {
+            pos.x = 5;
+            Translation = pos;
+        }
+    }
 
     public override void _PhysicsProcess(float delta)
     {
-        Vector3 direction = Vector3.Zero;
+        ConstraintsPlayerMove();
+        
+        /*Vector3 direction = Vector3.Zero;
 
         if (Input.IsActionPressed("ui_left")) direction.x -= 1;
         if (Input.IsActionPressed("ui_right")) direction.x += 1;
         if (Input.IsActionPressed("ui_down")) direction.z += 1;
-        if (Input.IsActionPressed("ui_up")) direction.z -= 1;
+        if (Input.IsActionPressed("ui_up")) direction.z -= 1;*/
 
-        if(direction!=Vector3.Zero)
+        if (velocity!=Vector3.Zero) // в этом блоке было direction
         {
-            direction = direction.Normalized();
-            spatialPivot.LookAt(Translation + direction, Vector3.Up);
+            velocity = velocity.Normalized();
+            spatialPivot.LookAt(Translation + velocity, Vector3.Up);
         }
 
-        velocity.x = direction.x * speed;
-        velocity.z = direction.z * speed;
-        velocity.y -= fallAcceleration * delta;//персонаж должен постоянно немножко долбиться об пол :-)
+        velocity.x *= speed;
+        velocity.z *= speed;
+
+        if(!IsOnFloor()) velocity.y -= 100 * delta;//персонаж должен постоянно немножко долбиться об пол :-)
                                                //потому что столкновения определяются только в движении
 
-        if (IsOnFloor() && Input.IsActionJustPressed("ui_jump"))
+        if (isJumping)//(Input.IsActionJustPressed("ui_jump"))
         {
-            velocity.y += jumpImpuls;
+            velocity.y += 100;
+            isJumping = false;
         }
         
+        
+        //else 
+
         velocity = MoveAndSlide(velocity,Vector3.Up);//сглаживает столкновения, сбрасывая скорость, чтобы не дать
                                                      //персонажу "пробить" пол, или стены и т.п.
 
@@ -80,7 +158,7 @@ public class KinematicBody_Player : KinematicBody
                 }
             }
         }
-        if(direction != Vector3.Zero)
+        if(velocity.z!=0 || velocity.x!=0) //вместо velocity было direction
         {
             GetNode<AnimationPlayer>("AnimationPlayer").PlaybackSpeed = 4;
         }
--- Main.cs
+++ Main.cs
@@ -4,21 +4,31 @@ using System;
 public class Main : Node
 {
     [Export] public PackedScene mobScene;
+    private Control rect;
 
 
     public override void _Ready()
     {
         GD.Randomize();
-        GetNode<Control>("Control/ColorRect").Hide();
+        rect = GetNode<Control>("Control/ColorRect");
+        rect.Hide();
     }
 
-    public override void _UnhandledInput(InputEvent @event)
+    public override void _Input(InputEvent iE)
+    {
+        if((iE is InputEventScreenTouch sT) && rect.Visible)
+        {
+            GetTree().ReloadCurrentScene();
+        }
+    }
+
+    /*public override void _UnhandledInput(InputEvent @event)
     {
         if(@event.IsActionPressed("ui_accept") && GetNode<Control>("Control/ColorRect").Visible)
         {
             GetTree().ReloadCurrentScene();
         }
-    }
+    }*/
 
     public void OnPlayerHit()
     {
